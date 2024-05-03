a55b08a5aa33232d0ab9196a351a3e2896e51e2d
iamcarlosamaya <cje.amaya@gmail.com>
2024-05-02 20:29:56 -0400

    Added UI for user to play

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Welcome to Rock, Paper, Scissors!")
+//console.log("Welcome to Rock, Paper, Scissors!")
 
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
@@ -69,6 +69,12 @@ function playRound(humanSelection, computerSelection) {
     }
 }
 
+function updateScore() {
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+}
+
+/*
 function playGame(numberOfGames) {
     for (var i = 1; i <= numberOfGames; i++) {
         console.log("Round " + i);
@@ -106,6 +112,9 @@ function playGame(numberOfGames) {
     }
 }
 
+// playGame(numberOfGames);
+*/
+
 // Select Games
 const numberOfGames = 5;
 
@@ -113,6 +122,68 @@ const numberOfGames = 5;
 var humanScore = 0;
 var computerScore = 0;
 
-playGame(numberOfGames);
+document.addEventListener("DOMContentLoaded", function() {
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    const playerScoreDisplay = document.getElementById("player-score");
+    const computerScoreDisplay = document.getElementById("computer-score");
+
+    // Show outcome to user
+    const message = document.getElementById("game-outcome");
+
+    // Add Event listeners
+    const buttons = document.querySelectorAll('.player-panel button');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const myResponse = button.textContent;
+            
+            //console.log('Selection captured:', myResponse);
+
+            // Your turn
+            let humanSelection = myResponse.toLowerCase();
+            console.log("You selected: " + humanSelection)
+
+            // Computer's turn
+            let computerSelection = getComputerChoice();
+            console.log("Computer selects: " + computerSelection)
+
+            // Get result
+            const result = playRound(humanSelection, computerSelection);
+            console.log(result);
+
+            if (result === 1) {
+                humanScore++;
+                message.textContent = "You win!";
+            } else if (result === 2) {
+                computerScore++;
+                message.textContent = "Computer wins!";
+            } else {
+                message.textContent = "Tie!";
+            }
+
+            if (humanScore === 5) {
+                playerScoreDisplay.textContent = 0;
+                computerScoreDisplay.textContentn = 0;
+                message.textContent = "Game! You win!";
+            } else if (computerScore === 5) {
+                playerScoreDisplay.textContent = 0;
+                computerScoreDisplay.textContentn = 0;
+                message.textContent = "Game! Computer wins!";
+            }
+
+            // update scoreboard
+            playerScoreDisplay.textContent = humanScore;
+            computerScoreDisplay.textContent = computerScore;
+
+        });
+    });
+});
+
+// Get result
+//result = playRound(humanSelection, computerSelection);
+//console.log(result);
 
 console.log("Good luck out there!");
